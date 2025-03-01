9b44e0f94e44582543473fd3cf4514790eadb6fb
m.ruban <m.ruban@testportal.gov.ua>
2025-03-01 22:52:07 +0100

    task 01

--- 01.py
+++ 01.py
@@ -12,3 +12,23 @@ Sitarama Raju,1000
 файл і повертає загальну та середню суму заробітної плати всіх розробників.
 """
 
+import os
+
+
+def total_salary(path):
+    money_list = []
+    with open(path, mode="r", encoding="utf-8") as file:
+        for line in file.readlines():
+            money = float(line.strip().split(",")[-1])
+            money_list.append(money)
+    return (sum(money_list), sum(money_list) / len(money_list))
+
+
+fname = "./in/salary_file.txt"
+if os.path.isfile(fname):
+    total, average = total_salary(fname)
+    print(
+        f"Загальна сума заробітної плати: {total:.2f}, Середня заробітна плата: {average:.2f}"
+    )
+else:
+    print(f"File '{fname}' not found...")
